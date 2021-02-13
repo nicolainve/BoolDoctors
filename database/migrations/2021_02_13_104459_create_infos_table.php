728318cb8fd0fea04e0d193f808c5d3bc0e8ba61
Nicola <nginvernizzi1@gmail.com>
2021-02-13 11:57:07 +0100

    Info Migration

---
+++ database/migrations/2021_02_13_104459_create_infos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 150);
+            $table->string('surname', 150);
+            $table->string('slug', 150)->nullable();
+            $table->string('address', 30);
+            $table->text('CV');
+            $table->text('photo')->nullable();
+            $table->string('phone', 12);
+            $table->decimal('price', 6, 2);
+            $table->timestamps();
+
+            // Relation One-To-One
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infos');
+    }
+}
