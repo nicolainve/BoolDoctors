194b47a6ef6d3414de5a080c88cb8115748dee9a
Nicola <nginvernizzi1@gmail.com>
2021-02-23 10:52:07 +0100

    favicon red

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="icon" href="{{ asset('./img/stethoscope-solid.svg') }}">
+  <link rel="icon" href="{{ asset('./img/stethoscope-solid.png') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
