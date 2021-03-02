0e58e8210620197556b4a760dd11b299af51c673
Nicola <nginvernizzi1@gmail.com>
2021-03-02 16:41:40 +0100

    browser-sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,4 +16,11 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .options ({
         processCssUrls: false,
+    })
+    .browserSync({
+        proxy: 'http://127.0.0.1:8000',
+        open: false,
+        watch: true,
+        notify: false,
+        ui: false
     });
